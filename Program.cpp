8e702234b6ed00e0d1e95f1bb3d5178669370cba
ukaapple <uka.apple@gmail.com>
2018-08-21 19:28:03 +0900

    adjust cube num

--- Program.cpp
+++ Program.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    CubeInsWindow *pWnd = new CubeInsWindow(200, 200, 200);
+    CubeInsWindow *pWnd = new CubeInsWindow(100, 100, 100);
     pWnd->run();
     delete pWnd;
     return 0;
